83d3264e2c59daab26b79f6bb4ddafd5f3615ab9
Filip Cudny <filip96zx@gmail.com>
2021-12-06 10:35:38 +0100

    Updates

---
+++ precache-manifest.c888d9dc3e3aed40e58b2528c6cf757a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41051720b6a8fd37a9cd4aea9cd5764",
+    "url": "/DrinkBar/index.html"
+  },
+  {
+    "revision": "e9a4cba3b94c3849e9de",
+    "url": "/DrinkBar/static/css/main.7ef563af.chunk.css"
+  },
+  {
+    "revision": "ad3614cb375686e47f7b",
+    "url": "/DrinkBar/static/js/2.23a56cce.chunk.js"
+  },
+  {
+    "revision": "e364af75fff56a5298ee87761858e88e",
+    "url": "/DrinkBar/static/js/2.23a56cce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d367b73e464e43e0d65f",
+    "url": "/DrinkBar/static/js/3.f9672266.chunk.js"
+  },
+  {
+    "revision": "e9a4cba3b94c3849e9de",
+    "url": "/DrinkBar/static/js/main.07f1d726.chunk.js"
+  },
+  {
+    "revision": "08aff3767c69dd9aca43",
+    "url": "/DrinkBar/static/js/runtime-main.a6bd5c56.js"
+  },
+  {
+    "revision": "f77aba2de4dd1e19b3e5e78e158e948c",
+    "url": "/DrinkBar/static/media/background6kx8k.f77aba2d.jpg"
+  }
+]);
